6f48c951d67225b32e4122a24a7cdd620103ea24
EZabotkin <eugenezabotkin@gmail.com>
2022-09-25 13:27:49 +0300

    testcommit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   tagline: 'WEB3 Decentralized Framework',
   url: 'https://docs.thepower.io',
   baseUrl: '/',
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'thepower', // Usually your GitHub org/user name.
